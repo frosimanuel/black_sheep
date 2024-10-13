86354cf73d8919da6f26f26dc97f980ebe7b67ca
Manu <frosimanuel@gmail.com>
2024-10-13 03:58:26 -0300

    adding juno config

---
+++ juno.config.mjs
@@ -0,0 +1,9 @@
+import {defineConfig} from '@junobuild/config';
+
+/** @type {import('@junobuild/config').JunoConfig} */
+export default defineConfig({
+  satellite: {
+    id: 'lmsgt-myaaa-aaaal-ajo5a-cai',
+    source: 'dist'
+  }
+});
